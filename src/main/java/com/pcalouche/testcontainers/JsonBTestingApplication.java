813ce6c6e28f6830e59b02aad1ff3e53db49c530
Philip Calouche <philip.calouche@outlook.com>
2022-04-20 20:58:51 -0400

    [ci skip] misc cleanup

--- src/main/java/com/pcalouche/testcontainers/JsonBTestingApplication.java
+++ src/main/java/com/pcalouche/testcontainers/JsonBTestingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JsonBTestingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JsonBTestingApplication.class, args);
     }
